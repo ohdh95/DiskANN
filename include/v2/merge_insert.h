d9236673ca8a8ec542c35814c79e791fdb681d4a
Donghyun Oh <ohdh95@gmail.com>
2025-09-02 17:37:25 +0900

    진짜 id_disk_map 이용해서 search 가능

--- include/v2/merge_insert.h
+++ include/v2/merge_insert.h
@@ -102,7 +102,6 @@ namespace diskann {
     uint64_t _beamwidth;
     std::vector<uint32_t> id_disk_map;
 
->>>>>>> 9b09e96... id_disk_map 사용해서 search 가능
     std::unordered_map<unsigned, TagT> curr_location_to_tag;
 
     std::shared_ptr<Index<T, TagT>>    _mem_index_0 = nullptr;
